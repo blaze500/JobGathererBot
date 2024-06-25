0dcb33b45745aea7e393a950ed70a3a07eba77a6
Jaden Millington <57372238+blaze500@users.noreply.github.com>
2024-06-25 16:19:30 -0400

    Add files via upload

--- Webscraper_JobFinder.py
+++ Webscraper_JobFinder.py
@@ -24,33 +24,6 @@ class JobFinder:
         self.automate = False
 
     def LoginToJobs(self):
-
-        """
-        self.seleniumDriver.get(
-            "https://secure.indeed.com/auth?hl=en_US&co=US&continue=https%3A%2F%2Fwww.indeed.com%2F&tmpl=desktop&service=my&from=gnav-util-homepage&jsContinue=https%3A%2F%2Fwww.indeed.com%2F&empContinue=https%3A%2F%2Faccount.indeed.com%2Fmyaccess&_ga=2.216882070.1436526071.1662068128-19035690.1662068128")
-
-        time.sleep(7)
-        self.seleniumDriver.find_element(By.CSS_SELECTOR, "[name='__email']").send_keys("jobautomatorbottestacc@gmail.com")
-        button = self.seleniumDriver.find_element(By.CSS_SELECTOR, "[type='submit']")
-        self.seleniumDriver.execute_script("arguments[0].click();", button)
-        time.sleep(5)
-
-        try:
-            button = self.seleniumDriver.find_element(By.ID, "auth-page-google-password-fallback")
-            self.seleniumDriver.execute_script("arguments[0].click();", button)
-        except:
-            print("Was Unable To Click On The Put In Password Due To Captcha On Indeed")
-            wait = input("Press Enter to continue.")
-
-        time.sleep(5)
-        self.seleniumDriver.find_element(By.CSS_SELECTOR, "[name='__password']").send_keys("fakepassword")
-        button = self.seleniumDriver.find_element(By.CSS_SELECTOR, "[type='submit']")
-        self.seleniumDriver.execute_script("arguments[0].click();", button)
-
-        time.sleep(5)
-
-        """
-
         linkedInLoginFile = open('LinkedInLogin.txt', 'r', encoding="utf-8")
         linkedInLoginInfo = linkedInLoginFile.read().splitlines()
 
